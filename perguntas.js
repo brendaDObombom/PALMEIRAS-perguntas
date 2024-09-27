b81876db7a24575a6e813a6e3f1bf8d7889f22f4
Brenda Eduarda <148216936+brendaDObombom@users.noreply.github.com>
2024-09-27 20:51:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Palmeiras',
+    'Qual foi o primeiro nome do verdão?',
+    'Sociedade Esportiva Palestra Itália '
+)
+
+criaCartao(
+    'Palmeiras ',
+    'Quantos lugares cobertos tem o estádio?',
+    '42 mil lugares'
+)
+
+criaCartao(
+    'Palmeiras',
+    'Quantos títulos do campeonato paulista temos?',
+    '22 títulos '
+)
+
+criaCartao(
+    'Palmeiras',
+    'Quantas vitórias temos em cima do Corinthians? ',
+    '135 vitórias'
+)
